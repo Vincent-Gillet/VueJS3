a017d44a200969023aceea115e12bd74e9be8064
Vincent-Gillet <vincent.gillet.vg@gmail.com>
2024-04-26 10:42:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+
+
+<template>
+  <header>
+    <HelloWorld />
+
+
+    <div class="bold"
+         :class="[user.age > 30 ? 'text-red' : 'text-green']">
+        {{user.name}}
+    </div>
+
+
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const name = ref('NordCoders');
+const name = ref('NordCoders');
+
+const user = reactive({
+  name: 'NordCoders',
+      age: 30,
+});
+
+
+const display = () => {
+  console.log(user.name, user.age);
+}
+
+display()
+
+</script>
+
+<style scoped>
+
+.bold {
+  font-weight: bolder;
+}
+
+.text-red {
+  color:indianred;
+}
+
+.text-green {
+  color: green;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const props = defineProps(['user']);
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <slot name="name">
+
+  </slot>
+  <slot name="age"></slot>
+</template>
+
+<style scoped>
+
+
+</style>
